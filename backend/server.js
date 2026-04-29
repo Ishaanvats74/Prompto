80a0ab61f3fdfa60bff12d573e6256006000940e
Ishaan vats <ishaanvats74@gmail.com>
2026-04-29 07:37:56 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://prompto-rust.vercel.app", "*"],
+    origin: ["http://localhost:3000", "https://prompto-beta.vercel.app/", "*"],
     credentials: true,
   }),
 );
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://prompto-rust.vercel.app", "*"],
+    origin: ["http://localhost:3000", "https://prompto-beta.vercel.app/", "*"],
     methods: ["GET", "POST"],
     credentials: true,
   },
